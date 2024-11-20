113cba9b32e4dc7d645b1ea0ca1979c8a86e8dcb
DEV-TOP <haselmairo99@proton.me>
2024-11-20 11:30:44 -0600

    first commit

---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php
+
+
+	$managername = "master@star";
+	$managerpass = "1234QWERasdf";
+	$host = "localhost";
+	$user = "root"; 
+	$pwd = ""; 
+	$sql_db = "sjob_db"; 
+
+	
+	$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+?>
